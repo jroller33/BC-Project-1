e3bc8e9e6b2d5717753a3e6e662d2c5e525215b7
John Roller <jtroller33@gmail.com>
2022-07-06 11:53:18 -0500

    general structure done. Saving text to localStorage but doesn't persist on refresh

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,10 @@
+function store(){
+  var recipeInput= document.getElementById("recipeInput");
+  localStorage.setItem("recipeInput", recipeInput.value);
+ }
 
 
-
-/*
+/*      Dynamically Create HTML elements:
 function myFunction() {
     const newP = document.createElement("p");
     const textP = document.createTextNode("Hello");
