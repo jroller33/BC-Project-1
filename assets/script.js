dbd24338e9203286e866f6846f14ee12159b5543
John Roller <jtroller33@gmail.com>
2022-07-06 12:35:13 -0500

    using jquery for localstorage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
+var saveButton = $("#saveRecipeButton");
+saveButton.on('click', function (event) {
+  event.preventDefault();
+  var recipeInput = $('#recipeInput').val();
+  console.log(recipeInput);
+  localStorage.setItem("0", JSON.stringify(recipeInput));
+
+
+})
+
+
+/*
 function store(){
   var recipeInput= document.getElementById("recipeInput");
   localStorage.setItem("recipeInput", recipeInput.value);
  }
+
+*/
 
 
 /*      Dynamically Create HTML elements:
