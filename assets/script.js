d84e323f7aa544adf5ea9e1bbe0c725e8eccb71b
John Roller <jtroller33@gmail.com>
2022-07-06 18:29:56 -0500

    added YouTube API

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ recipe1tab.on('click', function (event) {
   event.preventDefault();
   var recipe1Input = $('#recipeInput').val();
   console.log(recipe1Input);
-  localStorage.setItem("1", JSON.stringify(recipe1Input));
+  localStorage.setItem('1', JSON.stringify(recipe1Input));
 
   })
 })
@@ -33,7 +33,7 @@ recipe2tab.on('click', function (event) {
   event.preventDefault();
   var recipe2Input = $('#recipeInput').val();
   console.log(recipe2Input);
-  localStorage.setItem("2", JSON.stringify(recipe2Input));
+  localStorage.setItem('2', JSON.stringify(recipe2Input));
 
 
   })
@@ -52,7 +52,7 @@ recipe3tab.on('click', function (event) {
   event.preventDefault();
   var recipe3Input = $('#recipeInput').val();
   console.log(recipe3Input);
-  localStorage.setItem("3", JSON.stringify(recipe3Input));
+  localStorage.setItem('3', JSON.stringify(recipe3Input));
 
 
   })
@@ -71,7 +71,7 @@ recipe4tab.on('click', function (event) {
     event.preventDefault();
     var recipe4Input = $('#recipeInput').val();
     console.log(recipe4Input);
-    localStorage.setItem("4", JSON.stringify(recipe4Input));
+    localStorage.setItem('4', JSON.stringify(recipe4Input));
   })
 })
 
@@ -88,12 +88,60 @@ recipe5tab.on('click', function (event) {
   event.preventDefault();
   var recipe5Input = $('#recipeInput').val();
   console.log(recipe5Input);
-  localStorage.setItem("5", JSON.stringify(recipe5Input));
+  localStorage.setItem('5', JSON.stringify(recipe5Input));
 
 
   })
 })
 
+
+
+//        YouTube API
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+// text to speech API Key: 9910c3d681d94aae8f78907df9e56a0b
+// 
 
 // saveButton.on('click', function (event) {
 //   event.preventDefault();
