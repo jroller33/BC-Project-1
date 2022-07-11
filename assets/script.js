1b1742e3b192ed4dffb3184ebfdb23eaa1b4863f
John Roller <jtroller33@gmail.com>
2022-07-10 22:18:29 -0500

    page layout fixed.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
- 
+
   //              YOUTUBE SEARCH AND DYNAMICALLY CREATE IFRAME
   // doSearch, get text in search input, make ajax call, populate divs with results
   const doSearch = () => {
@@ -10,8 +10,8 @@ $(document).ready(() => {
     // }
 
     // make the endpoint with query parameters (https://developers.google.com/youtube/v3/docs/search/list) part=snippet is required. maxResults=10 set the number of results we want to retrieve
-     let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=' + searchQuery;
-    
+    let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&key=' + searchQuery;
+
     // ajax request here setting the url, iframe will hold the first video result
     $.ajax({
       url: url,
@@ -28,19 +28,19 @@ $(document).ready(() => {
   };
 
   // click event on submit button, doSearch with input text
-    $("#youtubeSearchButton").on("click", function(event) {
-      event.preventDefault();
+  $("#youtubeSearchButton").on("click", function (event) {
+    event.preventDefault();
     //  console.log("search button");
-      doSearch();
+    doSearch();
   });
 
-    // on document ready doSearch with default input text
-    if ($("#youtubeSearchBar").attr("data-type") == "0") {
-      searchQuery = "pizza recipe";
-      console.log(searchQuery);
-      doSearch();
-     
-      $("#youtubeSearchBar").attr("data-type", "1")
+  // on document ready doSearch with default input text
+  if ($("#youtubeSearchBar").attr("data-type") == "0") {
+    searchQuery = "pizza recipe";
+    console.log(searchQuery);
+    doSearch();
+
+    $("#youtubeSearchBar").attr("data-type", "1")
   }
 
 
@@ -54,39 +54,38 @@ $(document).ready(() => {
     "headers": {
       "X-RapidAPI-Key": "",
       "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-    }      
+    }
   };
-  
+
   $.ajax(settings).done(function (response) {
-   // console.log(response);
-   // console.log(response.recipes[0].title);
-   // console.log(response.recipes[0].instructions);
+    // console.log(response);
+    // console.log(response.recipes[0].title);
+    // console.log(response.recipes[0].instructions);
     $('#recipeApiResults').append((response.recipes[0].title)) + '\n\n';
     $('#recipeApiResults').append(response.recipes[0].instructions);
   });
 
 
 
-    //             TABS, RECIPE TEXT AND SAVING TO LOCAL STORAGE
+  //             TABS, RECIPE TEXT AND SAVING TO LOCAL STORAGE
 
   const doTabCapture = (index) => {
-      $('#recipeInput').val("");
-      $('#recipeInput').val(JSON.parse(localStorage.getItem(index)));
-      currentTab.currentId = index;
+    $('#recipeInput').val("");
+    $('#recipeInput').val(JSON.parse(localStorage.getItem(index)));
+    currentTab.currentId = index;
   }
 
 
-    $(".tab").on("click",function (event) {
-      // event.preventDefault();
-      console.log("hello",$(this).attr("value"));
+  $(".tab").on("click", function (event) {
+    // event.preventDefault();
+    console.log("hello", $(this).attr("value"));
 
-      doTabCapture($(this).attr("value"));
-    })
-  
-    let currentTab = {currentId: 0, changed: false};
+    doTabCapture($(this).attr("value"));
+  })
 
-  if (!currentTab.changed)
-  {
+  let currentTab = { currentId: 0, changed: false };
+
+  if (!currentTab.changed) {
     doTabCapture(0)
     currentTab.changed = true;
   }
@@ -95,10 +94,10 @@ $(document).ready(() => {
   var saveRecipe1 = $("#saveRecipeButton");
   saveRecipe1.on('click', function (event) {
     event.preventDefault();
-    
+
     var recipe1Input = $('#recipeInput').val();
-  console.log("hello ", String(currentTab.currentId));
-  localStorage.setItem(String(currentTab.currentId), JSON.stringify(recipe1Input));
-  
+    console.log("hello ", String(currentTab.currentId));
+    localStorage.setItem(String(currentTab.currentId), JSON.stringify(recipe1Input));
+
   })
-  });
+});
